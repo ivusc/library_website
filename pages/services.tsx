9eb7929a86994f4075a3b56ffc5d2187cd25f7c6
ivusc <100025097+ivusc@users.noreply.github.com>
2022-11-07 23:08:12 +0800

    updated responsiveness

--- pages/services.tsx
+++ pages/services.tsx
@@ -18,14 +18,14 @@ export const getServerSideProps : GetServerSideProps = async () => {
 
 const Services : NextPage<IService> = ({ libraryData }) => {
   return (
-    <div className='mx-10 flex flex-col space-y-3 h-screen xl:mx-[10em]'>
+    <div className='mx-10 flex flex-col space-y-3 h-full min-h-screen xl:mx-[10em]'>
       <Heading>Sevices</Heading>
       <div className='w-full flex flex-col justify-center'>
         <Title classStyles='mt-5 dark:text-gray-300 text-gray-700'>Crowd Level</Title>
         {libraryData.map((library) => (
           <div className='grid grid-cols-12' key={library.name}>
-            <p className='col-span-3 font-fira text-lg'>{ library.name }</p>
-            <Progress progress={library.crowdLevel?.toString().concat('%')} classStyles={'w-[70%] col-span-9 '}/>
+            <p className='lg:col-span-3 col-span-12 font-fira text-lg'>{ library.name }</p>
+            <Progress progress={library.crowdLevel?.toString().concat('%')} classStyles={'lg:w-[70%] w-full lg:col-span-9 col-span-12 mt-3 lg:mt-0'}/>
           </div>
         ))}
       </div>
