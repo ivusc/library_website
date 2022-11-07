56e101962f9a8351fe6db828b455189d97688fb8
ivusc <100025097+ivusc@users.noreply.github.com>
2022-11-07 20:18:28 +0800

    changed production api url

--- pages/services.tsx
+++ pages/services.tsx
@@ -18,6 +18,7 @@ else if (env == "production"){
 }
 
 export const getServerSideProps : GetServerSideProps = async () => {
+  console.log(env)
   const response = await fetch(baseURL).then((res) => res.json());
   return {
     props: {
@@ -27,14 +28,14 @@ export const getServerSideProps : GetServerSideProps = async () => {
 }
 
 const Services : NextPage<IService> = ({ libraryData }) => {
-
+  console.log(env)
   return (
     <div className='mx-10 flex flex-col space-y-3 h-screen xl:mx-[10em]'>
       <Heading>Sevices</Heading>
       <div className='w-full flex flex-col justify-center'>
         <Title classStyles='mt-5 dark:text-gray-300 text-gray-700'>Crowd Level</Title>
         {libraryData.map((library) => (
-          <div className='grid grid-cols-12'>
+          <div className='grid grid-cols-12' key={library.name}>
             <p className='col-span-3 font-fira text-lg'>{ library.name }</p>
             <Progress progress={library.crowdLevel?.toString().concat('%')} classStyles={'w-[70%] col-span-9 '}/>
           </div>
