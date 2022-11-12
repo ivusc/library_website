c8cf6ad825fa3fdebce2f3b9ded61b7c1bb2d5fe
ivusc <100025097+ivusc@users.noreply.github.com>
2022-11-12 14:04:08 +0800

    bug fixes

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -9,7 +9,7 @@ const Services : NextPage = () => {
     <div className='mx-10 flex flex-col space-y-3 h-full min-h-screen xl:mx-[10em]'>
       <Heading classStyles='text-center'>Sevices</Heading>
       <div className='flex flex-col items-center justify-center min-h-[70vh]'>
-        <div className='w-[40%] gap-4 grid grid-cols-2 '>
+        <div className=' lg:w-[60%] md:w-[80%] w-full xl:w-[40%] gap-4 grid md:grid-cols-2 grid-cols-1'>
           {ServiceItems.map((item) => <Card item={item} key={item.title}/>)}
         </div>
       </div>
